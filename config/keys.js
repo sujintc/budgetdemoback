86da5066d82da712ebf1d6febfda249459b2d259
sujintc <133184229+sujintc@users.noreply.github.com>
2024-05-10 10:19:04 +0530

    Delete config directory

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-export const secretOrKey = "SureshRaina";
-export const mongoURI= "mongodb+srv://tcsujin99:XPnLMYqJT9rF7WBt@cluster0.h7jttdq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-
-// XPnLMYqJT9rF7WBt
